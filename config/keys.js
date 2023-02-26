31d625c81dfb6451f090bb6ffb4938a9ecd01c6c
einstalek <arganaidi@phystech.edu>
2023-02-26 23:44:23 +0200

    fixes

--- config/keys.js
+++ config/keys.js
@@ -1,13 +1,7 @@
-console.log("=".repeat(60));
-console.log(process.env.NODE_ENV);
-console.log("=".repeat(60));
-
 // keys.js - figure out what set of credentials to return
 if (process.env.NODE_ENV === 'production') {
-    console.log("Exporting PROD keys");
     module.exports = require('./prod');
 } else {
-    console.log("Exporting DEV keys");
     module.exports = require('./dev');
 }
   
