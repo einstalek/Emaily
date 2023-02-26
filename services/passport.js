cb70342c7d64e5db2bf1ab4dd9e1e079f8d2ef3a
einstalek <arganaidi@phystech.edu>
2023-02-26 23:41:47 +0200

    fixes

--- services/passport.js
+++ services/passport.js
@@ -1,7 +1,7 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
-const keys = require(__dirname + '/../config/dev');
+const keys = require(__dirname + '/../config/keys');
 
 const mongoose = require("mongoose");
 const User = mongoose.model('users');
